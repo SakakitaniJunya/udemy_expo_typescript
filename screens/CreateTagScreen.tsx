0f32de1faa445b8c2c56531e43a58bd900f87b17
sakaki <sakajun02080@gmail.com>
2022-01-24 22:17:29 +0900

    update 01_24

---
+++ screens/CreateTagScreen.tsx
@@ -0,0 +1,40 @@
+import React, { VFC } from 'react';
+import tw from 'tailwind-rn';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types/types';
+import { useCreateTag } from '../hooks/useCreateTag';
+import { InputField } from '../components/InputField';
+import { Title } from '../components/Title';
+import { IconButton } from '../components/IconButton';
+
+type Props = {
+  navigation: NativeStackNavigationProp<RootStackParamList, 'CreateTag'>;
+};
+
+export const CreateTagScreen: VFC<Props> = ({ navigation }) => {
+  //画面遷移、値渡し
+  const { createErr, name, setName, createTag } = useCreateTag({
+    navigation,
+  });
+  return (
+    <SafeAreaView style={tw('flex-1 bg-gray-100 items-center')}>
+      <View style={tw('flex-1 bg-gray-100 items-centers')}>
+        {/* 要素として二つ持ち、うち右はダミーの要素 */}
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <AntDesign name="close" size={25} color="gray" />
+        </TouchableOpacity>
+        <View />
+      </View>
+      <Title first="New" last="Tag" />
+      <InputField
+        leftIcon="tag"
+        placeholder="Tag name"
+        autoFocus
+        value={name}
+        onChangeText={(text: string) => setName(text)}
+      />
+    </SafeAreaView>
+  );
+};
